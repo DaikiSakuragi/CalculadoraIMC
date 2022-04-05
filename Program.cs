64046d4a0afc7af17af14a8df9667668483151cb
Daiki Sakuragi <84183610+DaikiSakuragi@users.noreply.github.com>
2022-04-05 18:11:12 -0300

    Criando Calculadora IMC

--- Program.cs
+++ Program.cs
@@ -6,7 +6,19 @@ namespace CalculadoraIMC
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Entre com seu nome!");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Entre com sua altura!");
+            float altura = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("Entre com seu peso!");
+            float peso = Convert.ToSingle(Console.ReadLine());
+
+            double IMC = peso / (altura * altura);
+
+            Console.WriteLine("Você {0} de altura {1}cm e de peso {2}kg possui um IMC de: {3} ", nome, altura, peso, IMC); 
+
         }
     }
 }
